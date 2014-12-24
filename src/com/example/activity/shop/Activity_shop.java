095aa669b43b8781f0c59465e910e405fbddcc67
LiesLee <LiesLee@foxmail.com>
2014-12-24 21:10:20 +0800

    。。。。

--- src/com/example/activity/shop/Activity_shop.java
+++ src/com/example/activity/shop/Activity_shop.java
@@ -1,14 +1,43 @@
 package com.example.activity.shop;
 
+import com.android.volley.VolleyError;
+import com.example.activity.common.BaseActivity;
 import com.example.keyguard.R;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Activity_shop extends Activity{
+import org.json.JSONObject;
+
+public class Activity_shop extends BaseActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab_shop);
 	}
+
+    @Override
+    public <T> void onHttpSuccess(long flag, JSONObject jsonString, T response) {
+
+    }
+
+    @Override
+    public void onHttpError(long flag, VolleyError error) {
+
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public String setTag() {
+        return null;
+    }
 }
