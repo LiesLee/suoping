f4b5623bd6ad48145a669fe6a851978d71666b93
qinxianyuzou <qinxianyuzou@qinxianyuzou_pc>
2014-12-24 20:43:02 +0800

    添加log开关

--- src/com/example/util/LogUtil.java
+++ src/com/example/util/LogUtil.java
@@ -14,6 +14,8 @@
 package com.example.util;
 
 import android.util.Log;
+
+import com.example.http.Config;
 /**
  * 全局日志管理类<BR>
  * [功能详细描述]
@@ -23,39 +25,39 @@ public class LogUtil {
 	public static final String TAG = LogUtil.class.getSimpleName();
 
 	public static void d(String tag, String msg) {
-		//if (Config.DEBUG) {
+		if (Config.DEBUG) {
 			Log.d(tag, msg);
-		//}
+		}
 	}
 
 	public static void i(String tag, String msg) {
-		//if (Config.DEBUG) {
+		if (Config.DEBUG) {
 			Log.i(tag, msg);
-		//}
+		}
 	}
 
 	public static void w(String tag, String msg) {
-		//if (Config.DEBUG) {
+		if (Config.DEBUG) {
 			Log.w(tag, msg);
-		//}
+		}
 	}
 
 	public static void e(String tag, String msg) {
-		//if (Config.DEBUG) {
+		if (Config.DEBUG) {
 			Log.e(tag, msg);
-		//}
+		}
 	}
 
 	public static void v(String tag, String msg) {
-		//if (Config.DEBUG) {
+		if (Config.DEBUG) {
 			Log.v(tag, msg);
-		//}
+		}
 	}
 
 	public static void log(Throwable t) {
-		//if (Config.DEBUG && t != null) {
+		if (Config.DEBUG && t != null) {
 			t.printStackTrace();
-		//}
+		}
 	}
 
 }
