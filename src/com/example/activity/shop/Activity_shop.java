8624d6d523a211add464c940f63225f26e4c6a2b
qinxianyuzou <qinxianyuzou@qinxianyuzou_pc>
2014-12-25 22:41:52 +0800

    添加兑换页面，以及listview

--- src/com/example/activity/shop/Activity_shop.java
+++ src/com/example/activity/shop/Activity_shop.java
@@ -1,9 +1,14 @@
 package com.example.activity.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.android.volley.VolleyError;
 import com.example.activity.common.BaseActivity;
 import com.example.keyguard.R;
+import com.lidroid.xutils.BitmapUtils;
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.bitmap.PauseOnScrollListener;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 import android.app.Activity;
@@ -25,6 +30,12 @@ public class Activity_shop extends BaseActivity {
 	@ViewInject(R.id.lv_shop_body)
 	private ListView lv_shop_body;
 
+	private Shop_Adapter adapter;
+	/** 图片加载工具 */
+	private BitmapUtils bitmapUtils;
+	/** 保存数据源 */
+	private List<String> dataList = new ArrayList<String>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,11 +48,19 @@ public class Activity_shop extends BaseActivity {
 	@Override
 	protected void initUI() {
 		tv_public_top_title.setText("兑换");
+		bitmapUtils = new BitmapUtils(activity);
+		adapter = new Shop_Adapter(activity, bitmapUtils);
+		// 滑动时停止加载图片
+		lv_shop_body.setOnScrollListener(new PauseOnScrollListener(bitmapUtils, false, true));
+		lv_shop_body.setAdapter(adapter);
 	}
 
 	@Override
 	protected void initData() {
-
+		for (int i = 0; i < 20; i++) {
+			dataList.add("");
+		}
+		adapter.setData(dataList);
 	}
 
 	@Override
---
+++ src/com/example/activity/shop/Shop_Adapter.java
@@ -0,0 +1,111 @@
+package com.example.activity.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.keyguard.R;
+import com.lidroid.xutils.BitmapUtils;
+import com.lidroid.xutils.bitmap.BitmapDisplayConfig;
+import com.lidroid.xutils.bitmap.callback.BitmapLoadCallBack;
+import com.lidroid.xutils.bitmap.callback.BitmapLoadFrom;
+import com.lidroid.xutils.bitmap.callback.DefaultBitmapLoadCallBack;
+
+/**
+ * @Description 兑换的adapter
+ * @author Created by qinxianyuzou on 2014-12-25.
+ */
+public class Shop_Adapter extends BaseAdapter {
+	private Context mContext;
+	private LayoutInflater listContainer;
+	/** 数据源 */
+	private List<String> listData = new ArrayList<String>();
+	/** 图片加载工具 */
+	private BitmapUtils bitmapUtils;
+	/** 图片加载回调 */
+	private BitmapLoadCallBack<ImageView> bitmapLoadCallBack;
+
+	public Shop_Adapter(Context context, BitmapUtils bitmapUtils) {
+		mContext = context;
+		listContainer = LayoutInflater.from(mContext);
+		this.bitmapUtils = bitmapUtils;
+		bitmapLoadCallBack = new DefaultBitmapLoadCallBack<ImageView>() {
+
+			@Override
+			public void onLoadFailed(ImageView container, String uri, Drawable drawable) {
+				// TODO Auto-generated method stub
+				super.onLoadFailed(container, uri, drawable);
+				 container.setImageResource(R.drawable.item_icon);
+			}
+
+			@Override
+			public void onLoadCompleted(ImageView container, String uri, Bitmap bitmap, BitmapDisplayConfig config,
+					BitmapLoadFrom from) {
+				// TODO Auto-generated method stub
+				super.onLoadCompleted(container, uri, bitmap, config, from);
+			}
+		};
+		// ViewUtils.inject((Activity) mContext);
+	}
+
+	public void setData(List<String> data) {
+		listData = data;
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return listData.size();
+		// return 15;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return listData.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// 实例化viewholader
+		final HolderView listItemView;
+
+		if (convertView == null) {
+			listItemView = new HolderView();
+			//
+			convertView = listContainer.inflate(R.layout.item_shop, null);
+			listItemView.iv_item_shop_icon = (ImageView) convertView.findViewById(R.id.iv_item_shop_icon);
+			listItemView.tv_item_shop_name = (TextView) convertView.findViewById(R.id.tv_item_shop_name);
+			listItemView.tv_item_shop_money = (TextView) convertView.findViewById(R.id.tv_item_shop_money);
+			//
+			// 设置标记
+			convertView.setTag(listItemView);
+		} else {
+			listItemView = (HolderView) convertView.getTag();
+		}
+		listItemView.tv_item_shop_name.setText("优惠卷");
+		listItemView.tv_item_shop_money.setText("￥0.50");
+		bitmapUtils.display(listItemView.iv_item_shop_icon, "", bitmapLoadCallBack);
+		return convertView;
+	}
+
+	private class HolderView {
+		private ImageView iv_item_shop_icon;
+		private TextView tv_item_shop_name;
+		private TextView tv_item_shop_money;
+	}
+
+}
