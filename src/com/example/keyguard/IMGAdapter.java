980d5e213edf291f73650f56fd33d0feabfcca63
qinxianyuzou <qinxianyuzou@qinxianyuzou_pc>
2014-12-27 22:59:23 +0800

    锁屏添加了个很丑的界面，以及多了几个不知道干什么的类

---
+++ src/com/example/keyguard/IMGAdapter.java
@@ -0,0 +1,72 @@
+package com.example.keyguard;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+
+public class IMGAdapter  extends SimpleAdapter{
+	
+	Context context = null;
+	
+	private int lineHeight;
+	private int lineWidth;
+	
+	public void setlineHeight(int lineHeight) {  
+        Log.d("hhz", "setlineHeight..." + lineHeight);  
+        this.lineHeight = lineHeight;  
+    }
+	
+	public void setlineWidth(int lineWidth) {  
+        Log.d("hhz", "setlineWidth..." + lineWidth);  
+        this.lineWidth = lineWidth;  
+    }
+	 
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		// Inflate the note_item layout manually, and treat it as the item view
+		// 重新填充note_item部局，并把它作为项的view返回
+		convertView = LayoutInflater.from(context).inflate(R.layout.scroll_item, null);
+		HashMap<String, Object> theMap = (HashMap<String, Object>)getItem(position);
+		ImageView image = (ImageView)convertView.findViewById(R.id.id_title);
+		image.setImageResource((Integer) theMap.get("Image"));
+		// Calculate the item width by the column number to let total width fill the screen width
+		// 根据列数计算项目宽度，以使总宽度尽量填充屏幕
+		//int itemWidth = (int)(getResources().getDisplayMetrics().widthPixels -  colnum * 10)  / colnum;
+		// Calculate the height by your scale rate, I just use itemWidth here
+		// 下面根据比例计算您的item的高度，此处只是使用itemWidth
+		//int itemHeight = itemWidth;
+
+		/*
+	    AbsListView.LayoutParams param = new AbsListView.LayoutParams(
+	    		screenWidth-image_slide_rightMargin-image_slide_leftMargin,
+	    		(screenHeight-( image_slide_bottomMargin-image_slide_width)*2)*1);*/
+		  if(position%2==0 && position>=0){
+				convertView.setBackgroundColor(Color.YELLOW);
+		  }else{
+			  	convertView.setBackgroundColor(Color.GREEN);
+		 }
+	    AbsListView.LayoutParams param = new AbsListView.LayoutParams(lineWidth,lineHeight);		
+	    convertView.setLayoutParams(param);
+
+	    return convertView;
+	}
+
+	public IMGAdapter(Context context,
+			List<? extends Map<String, ?>> data, int resource,
+			String[] from, int[] to) {
+		super(context, data, resource, from, to);
+		this.context = context;
+	}
+
+}
---
+++ src/com/example/keyguard/UnRollListView.java
@@ -0,0 +1,55 @@
+package com.example.keyguard;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ListView;
+
+/**
+ * 
+ * @Description: scrollview中内嵌listview的简单实现
+ * 
+ * @File: ScrollViewWithListView.java
+ * 
+ * @Paceage com.meiya.ui
+ * 
+ * 
+ * @Date 下午03:02:38
+ * 
+ * @Version
+ */
+public class UnRollListView extends ListView {
+
+	public UnRollListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+	}
+
+	/**
+	 * 设置不滚动
+	 */
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, expandSpec);
+
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// 重写的onTouchEvent回调方法
+		switch (event.getAction()) {
+		// 按下
+		case MotionEvent.ACTION_DOWN:
+			return super.onTouchEvent(event);
+			// 滑动
+		case MotionEvent.ACTION_MOVE:
+			break;
+		// 离开
+		case MotionEvent.ACTION_UP:
+			return super.onTouchEvent(event);
+		}
+		// 注意：返回值是false
+		return false;
+	}
+
+}
