abd1372f4e7d1b993dcf8641eff16e8afb2a2141
LiesLee <LiesLee@foxmail.com>
2014-12-29 23:45:14 +0800

    >...

--- src/com/example/activity/earnings/Activity_newcomer.java
+++ src/com/example/activity/earnings/Activity_newcomer.java
@@ -3,11 +3,16 @@ package com.example.activity.earnings;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.android.volley.VolleyError;
+import com.example.activity.common.BaseActivity;
 import com.example.activity.common.KeyGuardActivityManager;
 import com.example.keyguard.R;
 
-public class Activity_newcomer extends Activity {
+import org.json.JSONObject;
+
+public class Activity_newcomer extends BaseActivity {
 
     public static void luanch(Activity activity) {
         Intent intent = new Intent(activity, Activity_newcomer.class);
@@ -18,5 +23,35 @@ public class Activity_newcomer extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newcomer);
+    }
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public <T> void onHttpSuccess(long flag, JSONObject jsonString, T response) {
+
+    }
+
+    @Override
+    public void onHttpError(long flag, VolleyError error) {
+
+    }
+
+    @Override
+    public String setTag() {
+        return this.getClass().getSimpleName();
     }
 }
